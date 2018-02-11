06631d0503a7e0079732a7be0dea6779192f80a8
hhd14 <32616937+hhd14@users.noreply.github.com>
2018-02-11 11:05:55 -0500

    Update TBparsing.h

--- TBparsing.h
+++ TBparsing.h
@@ -1,4 +1,3 @@
-// Header to outline all tasks related to parsing
 #ifndef _PARSE_H
 #define _PARSE_H
 
@@ -6,22 +5,22 @@
 // Whoever calls this function must also free the memory
 char* GetInput();
 
+// Resolves all paths in the command argument array
+char** PathResolve(char** args);
+
+// Returns the input string from the user with the proper whitespace formating.
+// Only one whitespace will be between each argument with no leading or trailing whitespace.
+char* PWhitespace(char* input);
+
 // Returns an array of c-strings (with each argument in a different slot)
 // All cells are dynamic, Whoever calls this function must also free the memory
 char** ParseI(char* input);
 
-// Returns the input string from the user with proper whitespace formating.
-// Only one whitespace will be present between each argument with no leading or trailing whitespace.
-char* PWhitespace(char* input);
-
-// Converts the line input from the user into the array of command arguments.
-// Dynamic memory allocated in this function. Whoever calls this function must also free the memory
-char** PArguments(char* input);
-
-// Resolves all paths in the command argument array
-char** PathResolve(char** args);
-
 // Expands all environment variables in the command argument array
 char** Expand(char** args);
+
+// Converts the line input from the user into the array of command arguments.
+// Dynamic memory is allocated in this function. Whoever calls this function must also free the memory
+char** PArguments(char* input);
 
 #endif
